ac64f961d5c125beff04a55e83aea6d4c1229a62
1475466469 <1475466469@qq.com>
2019-11-25 08:45:35 +0800

    "增加Jpa"

--- src/test/java/com/shop/shop/ShopApplicationTests.java
+++ src/test/java/com/shop/shop/ShopApplicationTests.java
@@ -2,14 +2,12 @@ package com.shop.shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
 @SpringBootTest
-public class ShopApplicationTests {
+class ShopApplicationTests {
 
     @Test
- public    void contextLoads() {
+    void contextLoads() {
     }
 
 }
